7d42242eebfb9159831d7762489c536fec41b3df
Lrosario26 <70249530+Lrosario26@users.noreply.github.com>
2020-09-11 02:32:37 -0400

    Add files via upload

---
+++ JavaIntro.js
@@ -0,0 +1,16 @@
+var firstName = "Luis";
+var lastName = " Rosario";
+var age = "24";
+alert("My name is " + firstName + lastName);
+
+alert("I am " + age + " years old");
+
+if (age > 21) {
+  alert("I am legally able to drink in the states.");
+} else if (age = 21) {
+  alert("I am just barely able to drink legally in the states.");
+} else if (age < 21) {
+  alert("It is illegal for me to drink in the states.");
+}
+
+console.log("I'm enjoying this class so far and I'm learning a lot");
